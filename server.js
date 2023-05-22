16727d332f1e54b83e5af3aa5375a8b214389343
Ahmed Usama <ahmedusamasaad@gmail.com>
2023-05-22 14:40:05 +0200

    reset

--- server.js
+++ server.js
@@ -6,21 +6,24 @@ require('dotenv').config();
 require('./connection')
 const server = http.createServer(app);
 
+
+
 const User = require('./models/User');
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const imageRoutes = require('./routes/imageRoutes');
 
-// Specify the allowed origins
-app.use(cors({
-    origin: 'https://ecommern-3pwp.onrender.com' // Replace with the appropriate origin
-}));
+
+
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/products', productRoutes);
 app.use('/images', imageRoutes);
 
+
+
 server.listen(8000, () => {
     console.log('server running at port', 8000)
-});
+})
